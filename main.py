fbb22ffa50930b24a8675f350f4ae86b8914d3e0
136S-Pfrimmer <136-rpfrimmer@sd43.bc.ca>
2023-05-23 12:22:44 -0700

    Updated Code (finally)

--- main.py
+++ main.py
@@ -1,35 +1,61 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-x,y = 1,1
-a,b = 4,3
-c = np.sqrt(x**2 + y**2)
+np.seterr(invalid='ignore') # one of tl or tr will probably be invalid, so i just dont really care that much
 
-if x <= 0 or y <= 0 or c > a+b:
-  print("ERR: Coordinates out of range of end effector.")
+### USER INPUT PARAMETERS
+sa = 3
+sb = 3
+sc = 3
+d = 8
 
-B = (np.arccos((a**2+c**2-b**2)/(2*a*c)) - np.arctan(y/x)) * -1
-C = np.pi - np.arccos((a**2 + b**2 - c**2)/(2*a*b)) + B
+### MATH
+o = d - sb
+r1 = sa**2
+r2 = sc**2
+ix = (-r2 + r1 + o**2) / (2*o)
+iy = np.sqrt(r1 - ix**2)
+tl = np.sqrt(r2 - r1)
+tr = np.sqrt(r1 - r2)
+theta1 = (np.arcsin(iy/sa)*(180/np.pi) if ((o > tl) or (np.isnan(tl))) else 180-np.arcsin(iy/sa)*(180/np.pi)) # Use arcsin result unless offset surpasses some value (tl or tr), in which case use 180-arcsin. This is because after you cross this threshold, the required angle will be obtuse, but of course arcsin can only produce acute angles.
+theta2 = (np.arcsin(iy/sc)*(180/np.pi) if ((o > tr) or (np.isnan(tr))) else 180-np.arcsin(iy/sc)*(180/np.pi))
 
-print(np.degrees(B),np.degrees(C))
+### CURSOR OBJECT FOR GRAPHING
+class Cursor:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.angle = 0
+    def line(self, length: int, angle: int):
+        self.angle += angle # rotate cursor
+        x1 = self.x
+        y1 = self.y
+        x2 = length * np.cos(np.radians(self.angle)) + self.x
+        y2 = length * np.sin(np.radians(self.angle)) + self.y
+        plt.plot([x1,x2],[y1,y2])
+        self.x = x2 # move cursor to end of new line segment
+        self.y = y2
+    def report(self,precision=3,name="",showerrors=False):
+        print("_________________")
+        print("Cursor Report: " + "("+name+")")
+        print("X: " + str(round(self.x,precision)))
+        print("Y: " + str(round(self.y, precision)))
+        print("Angle: " + str(round(self.angle,precision)))
+        if showerrors:
+            print("Errors: ",end="")
+            nominal = (True if (round(self.x,precision) == d) and (round(self.y,precision) == 0) else False)
+            if nominal:
+                print("No errors exist.")
+            else:
+                print("Errors exist at this precision. If precision is a large number, try making it smaller. There is some margin of error within this program. Current precision: " + str(precision))
+        print("_________________")
+        
+c = Cursor()
 
-fig = plt.figure("Inverse Kinematics 2D Test Space")
-ax = plt.gca()
-ax.set_aspect("equal")
-ax.grid()
-ax.set_ylim([-10, 10])
-ax.set_xlim([-10, 10])
-ax.plot(x, y, marker="o", markersize=12, markerfacecolor="green")
-mng = plt.get_current_fig_manager()
-mng.resize(*mng.window.maxsize())
-fig.show()
+c.line(sa,theta1)
+c.line(sb,-theta1)
+c.line(sc,-theta2)
+c.report(precision=3,name="Cursor",showerrors=True)
 
-def plot_point(point, angle, length):
-     x, y = point
-     endy = y + length * np.sin(angle)
-     endx = x + length * np.cos(angle)
-     ax.plot([x, endx], [y, endy], color="blue")
-     fig.show()
-     return round(endx,3),round(endy,3)
-
-print(plot_point(plot_point((0,0),B,a),C,b))
+plt.grid()
+plt.show()
